b19602e9dc4569ea17599a121d2314c66e9f79d6
anja220 <katrinesofiemn@hotmail.dk>
2022-05-17 16:34:15 +0200

    more stable model

--- utils.py
+++ utils.py
@@ -49,9 +49,9 @@ class DataLoader():
     def get_data_split(self):
         X = self.data.iloc[:,:-1]
         y = self.data.iloc[:,-1]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=2)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=2)
         # And validation data for ANN
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=2) # 0.25 x 0.8 = 0.2
+        X_test, X_val, y_test, y_val = train_test_split(X_train, y_train, test_size=0.50, random_state=2) # 0.25 x 0.8 = 0.2
         return X_train, X_test, y_train, y_test, X_val, y_val
     
     # Oversampling to allow the algorithm to see more of the minority group while training
