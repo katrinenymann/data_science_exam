e315fd1825e69ee8d45a4bca310d7679565ddb27
anja220 <katrinesofiemn@hotmail.dk>
2022-05-18 16:29:56 +0200

    wednesday work

--- utils.py
+++ utils.py
@@ -51,8 +51,8 @@ class DataLoader():
         y = self.data.iloc[:,-1]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=2)
         # And validation data for ANN
-        X_test, X_val, y_test, y_val = train_test_split(X_train, y_train, test_size=0.50, random_state=2) # 0.25 x 0.8 = 0.2
-        return X_train, X_test, y_train, y_test, X_val, y_val
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state= 8) # 0.25 x 0.8 = 0.2
+        return X_train, X_test, X_val, y_train, y_test, y_val
     
     # Oversampling to allow the algorithm to see more of the minority group while training
     def oversample(self, X_train, y_train):
